02636caa763219ddeba28e732e2268be5a40127a
ncgears <1918ncgears@gmail.com>
2022-02-28 16:18:20 -0500

    Initial Code for Rapid React

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,103 @@
+package frc.robot;
+
+public class Constants {
+    //IDs of things on the robot
+    public class IDs {
+        //IDs of Talons
+        public class Talon {
+            public static final int intake = 5;
+        }
+        //IDs of Victors
+        public class Victor {
+            public static final int driveLeftRear = 1;
+            public static final int driveLeftFront = 2;
+            public static final int driveRightRear = 3;
+            public static final int driveRightFront = 4;
+        }
+        //IDs of Solenoids
+        public class Solenoid {
+            public static final int driveLowGear = 0;
+            public static final int driveHighGear = 1;
+            public static final int collectorRaise = 2;
+        }
+    }
+
+    //Drivetrain
+    public class DriveTrain {
+        public class Left {
+            public static final boolean isInverted = false; //invert the left motors
+        }
+        public class Right {
+            public static final boolean isInverted = true; //invert the right motors
+        }
+    }
+
+    //Collector/Intake
+    public class Collector {
+        public static final boolean airStateDeployed = true; //state of the solenoid when collector deployed
+        public static final double kIntakeSpeed = 0.5; //Speed of intake motors
+        public static final boolean intakeIsInverted = false; //invert intake direction
+    }
+
+    //Auton
+    public class Auton {
+        public static final boolean isDisableld = false;
+        public static final String autonName = "Basic";
+    }
+
+    //Operator Interface
+    public class OI {
+        public static final int leftJoy = 0;
+        public static final int rightJoy = 1;
+        public static final double kMaxDeadband = 0.9;
+        public static final double kMinDeadband = 0.1;
+    }
+
+    //Controller Types (don't edit these)
+    public static final class Controllers {
+        public static final class Ultrastik { //Dual Ultimarc Utrastik 360
+            static final int BTN_1 = 1;
+            static final int BTN_2 = 2;
+            static final int BTN_3 = 3;
+            static final int BTN_4 = 4;
+            static final int BTN_5 = 5;
+            static final int BTN_6 = 6;
+            static final int BTN_7 = 7;
+            static final int BTN_8 = 8;
+            static final int BTN_9 = 9;
+            static final int BTN_10 = 10;
+            static final int BTN_11 = 11;
+            static final int BTN_12 = 12;
+            static final int BTN_13 = 13;
+            static final int BTN_14 = 14;
+            static final int BTN_15 = 15;
+        }
+        public static final class Logitech { //Logitech F310 Controller
+            static final int BTN_A = 1; //A Button
+            static final int BTN_B = 2; //B Button
+            static final int BTN_X = 3; //X Button
+            static final int BTN_Y = 4; //Y Button
+            static final int BTN_LB = 5; //Left Bumper (L1)
+            static final int BTN_RB = 6; //Right Bumper (R1)
+            static final int BTN_BACK = 7; //Back Button (Select)
+            static final int BTN_START = 8; //Start Button
+            static final int BTN_L = 9; //Left Stick Press (L3)
+            static final int BTN_R = 10; //Right Stick Press (R3)
+            static final int AXIS_LH = 0; //Left Analog Stick horizontal
+            static final int AXIS_LV = 1; //Left Analog Stick vertical
+            static final int AXIS_LT = 2; //Analog Left Trigger
+            static final int AXIS_RT = 3; //Analog Right Trigger
+            static final int AXIS_RH = 4; //Right Analog Stick horizontal
+            static final int AXIS_RV = 5; //Right Analog Stick vertical
+            static final int DPAD_UP = 0;
+            static final int DPAD_UPRIGHT = 45;
+            static final int DPAD_RIGHT = 90;
+            static final int DPAD_DNRIGHT = 135;
+            static final int DPAD_DN = 180;
+            static final int DPAD_DNLEFT = 225;
+            static final int DPAD_LEFT = 270;
+            static final int DPAD_UPLEFT = 315;
+            static final int DPAD_IDLE = -1; 
+        }
+    }
+}
---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,130 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+
+
+/**
+ * This drives the robot with Arcade steering (vs. tank drive) where the left joystick is throttle, and the right joystick is turning.
+ */
+public class Robot extends TimedRobot {
+  private final WPI_VictorSPX m_leftfront = new WPI_VictorSPX(Constants.IDs.Victor.driveLeftFront);
+  private final WPI_VictorSPX m_leftrear = new WPI_VictorSPX(Constants.IDs.Victor.driveLeftRear);
+  private final WPI_VictorSPX m_rightfront = new WPI_VictorSPX(Constants.IDs.Victor.driveRightFront);
+  private final WPI_VictorSPX m_rightrear = new WPI_VictorSPX(Constants.IDs.Victor.driveRightRear);
+  private final MotorControllerGroup m_leftMotor = new MotorControllerGroup(m_leftfront, m_leftrear);
+  private final MotorControllerGroup m_rightMotor = new MotorControllerGroup(m_rightfront, m_rightrear);
+  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
+  private final Joystick m_leftJoystick = new Joystick(Constants.OI.leftJoy);
+  private final Joystick m_rightJoystick = new Joystick(Constants.OI.rightJoy);
+  private final AHRS m_gyro = new AHRS(SPI.Port.kMXP);
+  private final DoubleSolenoid m_gearShift = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.IDs.Solenoid.driveLowGear, Constants.IDs.Solenoid.driveHighGear);
+  private final Solenoid m_collector = new Solenoid(PneumaticsModuleType.CTREPCM, Constants.IDs.Solenoid.collectorRaise);
+  private final WPI_TalonSRX m_intake = new WPI_TalonSRX(Constants.IDs.Talon.intake);
+  private final Timer m_timer = new Timer();
+
+  @Override
+  public void robotInit() {
+    // Invert one side only usually, so that forward is green on the controller and backwards is red
+    m_leftMotor.setInverted(Constants.DriveTrain.Left.isInverted);
+    m_rightMotor.setInverted(Constants.DriveTrain.Right.isInverted);
+    m_intake.setInverted(Constants.Collector.intakeIsInverted);
+  }
+
+  //This runs at when the robot is disabled. Useful for resetting things.
+  @Override
+  public void disabledInit() {
+    System.out.println("disabledInit: Resetting drivetrain to low gear");
+    m_gearShift.set(DoubleSolenoid.Value.kForward); //reset to low gear
+    System.out.println("disabledInit: Retracting collector");
+    m_collector.set(!Constants.Collector.airStateDeployed); //retract the collector
+  }
+
+  //This runs at the beginning of teleop
+  @Override
+  public void teleopInit() {
+  }
+
+  //This runs every loop during teleop
+  @Override
+  public void teleopPeriodic() {
+    // Drive with arcade drive.
+    // That means that the Y axis drives forward
+    // and backward, and the X turns left and right.
+    m_robotDrive.arcadeDrive(OI.deadband(-m_leftJoystick.getRawAxis(1)), OI.deadband(m_leftJoystick.getRawAxis(4)));
+    //Handle the gear shifting
+    if(m_leftJoystick.getRawButtonPressed(Constants.Controllers.Logitech.BTN_BACK)) { //shift to low gear
+      System.out.println("teleopPeriodic: Shifting to low gear -- I am STRONG!");
+      m_gearShift.set(DoubleSolenoid.Value.kForward);
+    } else if(m_leftJoystick.getRawButtonPressed(Constants.Controllers.Logitech.BTN_START)) { //shift to high gear
+      System.out.println("teleopPeriodic: Shifting to high gear -- I am SPEED!");
+      m_gearShift.set(DoubleSolenoid.Value.kReverse); 
+    }
+
+    //Handle the collector controls
+    if(m_leftJoystick.getRawButtonPressed(Constants.Controllers.Logitech.BTN_A)) { //lower collector
+      System.out.println("teleopPeriodic: Lowering Collector");
+      m_collector.set(!Constants.Collector.airStateDeployed);
+    } else if(m_leftJoystick.getRawButtonPressed(Constants.Controllers.Logitech.BTN_B)) { //raise collector
+      System.out.println("teleopPeriodic: Raising Collector");
+      m_collector.set(Constants.Collector.airStateDeployed);
+    }
+
+    //Handle the intake
+    if(m_leftJoystick.getRawButtonPressed(Constants.Controllers.Logitech.BTN_LB)) { //intake in
+      System.out.println("teleopPeriodic: Intake In");
+    } else if (m_leftJoystick.getRawButtonPressed(Constants.Controllers.Logitech.BTN_RB)) { //intake out
+      System.out.println("teleopPeriodic: Intake Out");
+    } else if (m_leftJoystick.getRawButtonReleased(Constants.Controllers.Logitech.BTN_LB) || m_leftJoystick.getRawButtonReleased(Constants.Controllers.Logitech.BTN_RB)) { //intake stop
+      System.out.println("teleopPeriodic: Intake Stop");
+    }
+    if(m_leftJoystick.getRawButton(Constants.Controllers.Logitech.BTN_LB)) { //intake in
+      m_intake.set(ControlMode.PercentOutput, Constants.Collector.kIntakeSpeed);
+    } else if (m_leftJoystick.getRawButton(Constants.Controllers.Logitech.BTN_RB)) { //intake out
+      m_intake.set(ControlMode.PercentOutput, Constants.Collector.kIntakeSpeed * -1);
+    } else { //intake stop
+      m_intake.set(ControlMode.PercentOutput, 0);
+    }
+  }
+
+  //This runs at the beginning of autonomous
+  @Override
+  public void autonomousInit() {
+    m_timer.reset();
+    m_timer.start();
+  }
+
+  //This runs every loop during autonomous
+  @Override
+  public void autonomousPeriodic() {
+    if (!Constants.Auton.isDisableld) {
+      switch (Constants.Auton.autonName) {
+        case "Basic": //Basic Auton
+          if (m_timer.get() < 2.0) { //2 seconds
+            m_robotDrive.arcadeDrive(0.5, 0.0); //drive forward at 50% speed
+          } else {
+            m_robotDrive.stopMotor(); //stop
+          }
+          break;
+        default:
+          break;
+      }
+    }
+  }
+}
